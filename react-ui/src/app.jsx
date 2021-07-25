afc555f712b176e34b8330767405635ebc6aa064
Avery Hampton <averydhampton@icloud.com>
2021-07-24 21:00:09 -0500

    adds bootstrap npm

--- react-ui/src/app.jsx
+++ react-ui/src/app.jsx
@@ -1,12 +1,17 @@
 import Footer from "./sectioning/footer/footer";
 import Header from "./sectioning/header/header";
 import Main from "./sectioning/main/main";
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// import { Button, Alert } from "react-bootstrap";
 
 function App() {
   return (
     <>
       <Header />
       <Main />
+      <Button>Test Button</Button>
       <Footer />
     </>
   );
